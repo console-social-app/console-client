88c54458e8ffa4437c967a36693cc88afb450019
ahcarter22 <86622845+ahcarter22@users.noreply.github.com>
2021-08-24 11:52:04 -0400

    Add posts axios requests to api folder

---
+++ src/api/posts.js
@@ -0,0 +1,49 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const createPost = (post, user) => {
+	return axios({
+		method: 'POST',
+		url: apiUrl + '/posts/',
+		headers: {
+			Authorization: `Bearer ${user.token}`,
+		},
+		data: {
+			post: {
+				owner: post.owner,
+				title: post.title,
+				content: post.content,
+			},
+		},
+	})
+}
+
+export const indexPosts = (user) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/posts/',
+		headers: {
+			Authorization: `Bearer ${user.token}`,
+		},
+	})
+}
+
+export const showPost = (user, postId) => {
+	return axios({
+		method: 'GET',
+		url: `${apiUrl}/posts/${postId}`,
+		headers: {
+			Authorization: `Bearer ${user.token}`,
+		},
+	})
+}
+
+export const deletePost = (user, postId) => {
+	return axios({
+		method: 'DELETE',
+		url: `${apiUrl}/posts/${postId}`,
+		headers: {
+			Authorization: `Bearer ${user.token}`,
+		},
+	})
+}
