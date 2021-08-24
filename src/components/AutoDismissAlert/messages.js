9140d04356b416c9e1267551fc308b5f4dce10c9
ahcarter22 <86622845+ahcarter22@users.noreply.github.com>
2021-08-24 11:45:55 -0400

    Add messages within AutoDismissAlert

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -1,7 +1,24 @@
-export const signUpSuccess = 'Succesfully registered! You\'ve been signed in as well.'
-export const signUpFailure = 'Registration failed. Email may be taken, or passwords don\'t match.'
+// AUTH
+export const signUpSuccess =
+	"Successfully registered! You've been signed in as well."
+export const signUpFailure =
+	"Registration failed. Email may be taken, or passwords don't match."
 export const signInSuccess = 'Welcome!'
-export const signInFailure = 'Failed to sign in. Check your email and password and try again.'
+export const signInFailure =
+	'Failed to sign in. Check your email and password and try again.'
 export const signOutSuccess = 'Come back soon!'
 export const changePasswordSuccess = 'Password changed successfully!'
-export const changePasswordFailure = 'Failed to change passwords. Check your old password and try again.'
+export const changePasswordFailure =
+	'Failed to change passwords. Check your old password and try again.'
+
+// POSTS
+export const createPostSuccess = 'Your post has successfully been created.'
+export const createPostFailure =
+	'Failed to create post, please double check your input and try again. Error: '
+export const indexPostsFailure =
+	"Couldn't find posts, please refresh the page and try again. Error: "
+export const showPostFailure =
+	"Couldn't find this post. Please refresh the page and try again. Error: "
+export const deletePostSuccess = 'Your post has successfully been deleted.'
+export const deletePostFailure =
+	'Something went wrong while deleting this post, please refresh and try again. Error: '
