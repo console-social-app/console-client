3ad0c27bc6511f0d271323b98db1240445a64e65
adamdeguire <adeguire443@gmail.com>
2021-08-24 12:54:11 -0500

    Add comment success/failure messages

---
+++ src/components/comments/CreateComment.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import { withRouter, Redirect } from 'react-router-dom'
+
+import { createComment } from '../../api/comments'
+import { createCommentSuccess, createCommentFailure } from '../AutoDismissAlert/messages'
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+class CreateComment extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      title: '',
+      director: '',
+      commentId: null
+    }
+  }
+
+  handleChange = (event) =>
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+  onCreateComment = (event) => {
+    event.preventDefault()
+
+    const { msgAlert, history, user } = this.props
+
+    createComment(this.state, user)
+      .then(res => this.setState({ commentId: res.data.comment._id }))
+      .then(() => history.push('/'))
+      .then(() => {
+        msgAlert({
+          heading: 'Comment Created',
+          message: createCommentSuccess,
+          variant: 'success'
+        })
+      })
+      .catch((err) => {
+        msgAlert({
+          heading: 'Couldn\'t Create Comment',
+          message: createCommentFailure + err.message,
+          variant: 'danger'
+        })
+      })
+  }
+
+  render () {
+    const { title, director, commentId } = this.state
+
+    if (commentId) {
+      return <Redirect to={
+        { pathname: '/comments/' + commentId }
+      } />
+    }
+
+    return (
+      <>
+        <h3>Create Comment</h3>
+        <Form onSubmit={this.onCreateComment}>
+          <Form.Group controlId='title'>
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              required
+              name='title'
+              value={title}
+              placeholder='Comment Title'
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId='director'>
+            <Form.Label>Director</Form.Label>
+            <Form.Control
+              required
+              name='director'
+              value={director}
+              placeholder='Director'
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Button variant='primary' type='submit'>Create</Button>
+        </Form>
+      </>
+    )
+  }
+}
+
+export default withRouter(CreateComment)
