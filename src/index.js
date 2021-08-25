d6af32df2f53077202dbef8e0cc7a6ad48c8831d
ans9611 <ans9611@gmail.com>
2021-08-25 00:23:35 -0400

    started worked on socket, chat Moon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
-
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import socketIOClient from 'socket.io-client'
+const baseURL = process.env.PUBLIC_URL
+const socket = socketIOClient(baseURL)
+
+socket.on('social', (message) => console.log(message))
 
 const appJsx = (
   <BrowserRouter basename={process.env.PUBLIC_URL}>
