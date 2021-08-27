8f9db637e4895becfadef9cdb3b491f35daa8c65
adamdeguire <adeguire443@gmail.com>
2021-08-27 14:44:27 -0500

    fix production url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://console-social-app.herokuapp.com/',
+  production: 'https://console-social-app.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
