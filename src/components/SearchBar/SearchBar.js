73683028fedf7ea69e3a1e7e175b69f1ca2b00b9
adamdeguire <adeguire443@gmail.com>
2021-08-27 01:09:46 -0500

    Implement User search bar with real-time results

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import apiUrl from '../../apiConfig'
+import { search } from '../../api/search'
+
+import './SearchBar.scss'
+
+class SearchBar extends Component {
+  state = {
+    users: null,
+    loading: false,
+    value: ''
+  };
+
+  search = async val => {
+    this.setState({ loading: true })
+    const results = await search(
+      `${apiUrl}/users?users=${val}`
+    )
+    console.log(results)
+    const users = results
+
+    this.setState({ users, loading: false })
+  }
+
+  onChangeHandler = async e => {
+    this.search(e.target.value)
+    this.setState({ value: e.target.value })
+  }
+
+  get renderUsers () {
+    let users = ''
+    if (this.state.users) {
+      console.log('it worked')
+      users = this.state.users.map(user => {
+        return (
+          <div className='result' key={user._id}>
+            <Link>{user.username}</Link>
+          </div>
+        )
+      })
+    }
+    if (!this.state.value) {
+      users = ''
+    }
+
+    return users
+  }
+
+  render () {
+    const { value } = this.state
+    const noResults = this.renderUsers.length === 0 && value.length > 0
+    const noResultsJsx = (<div className="result">No results</div>)
+    return (
+      <>
+        <div className="searchBar">
+          <input className="searchInput"
+            value={value}
+            onChange={e => this.onChangeHandler(e)}
+            placeholder="Search Users"
+          />
+        </div>
+        <div className="resultContainer">
+          {noResults ? noResultsJsx : this.renderUsers}
+        </div>
+      </>
+    )
+  }
+}
+
+export default SearchBar
